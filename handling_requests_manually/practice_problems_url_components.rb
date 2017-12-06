b68706143ac3972b7055617aa1ea377620d2a2e5
antoniozeus <constandinou.antonio@gmail.com>
2017-12-05 20:37:07 -0600

    LS Course 170

---
+++ handling_requests_manually/practice_problems_url_components.rb
@@ -0,0 +1,47 @@
+# URL
+https://amazon.com/Double-Stainless-Commercial-Refrigerator/B60HON32?ie=UTF8&qid=142952676&sr=93&keywords=commercial+fridge
+
+1. a) Identify Host
+      amazon.com
+1. b) Identify names of query parameters
+      ie, qid, sr, keywords
+1. c) Identify values of query parameters
+      UTF8, 142952676, 93, commercial+fridge
+1. d) Identify protocol
+      https
+1. e) Identify path
+      /Double-Stainless-Commercial-Refrigerator/B60HON32
+1. f) Identify port
+      This URL does not contain a port. Most software will use port 443 by default
+      when working with this URL due to it having a protocol of https, but that
+      information is not contained within the URL.
+
+2. Add port 3000 to the URL
+http://amazon.com":3000"/products/B60HON32?qid=142952676&sr=93
+
+3. Given the URL:
+http://localhost:4567/todos/15
+3. a) Identify query parameters
+      None
+   b) Identify protocol
+      http
+   c) Identify path
+      /todos/15
+   d) Identify host
+      localhost
+   e) Identify port
+      4567
+
+4. What are two different ways to encode a space in a query parameter? We
+   didnt cover this in the lectures, so feel free to search online for the answer.
+   1) the plus sign "+"
+   2) the %20 encoded
+
+5. What character indicates the beginning of a URLs query parameters?
+   the question mark "?"
+
+6. What character is used between the name and value of a query parameter?
+   the equal sign "="
+
+7. What character is used between multiple query parameters?
+   the ampersand sign "&"
--- handling_requests_manually/server.rb
+++ handling_requests_manually/server.rb
@@ -47,25 +47,69 @@ def parse_request(request_line)
   [http_method, path, params]
 end
 
-server = TCPServer.new("localhost", 3003)
-loop do
-  client = server.accept
+#server = TCPServer.new("localhost", 3003)         # used a port # we're not using
+#loop do
+#  client = server.accept # waits until a request is made to server -> accepts it -> opens connection to client
+#
+#  request_line = client.gets # using gets to get first line of whatever the request is
+#  next if !request_line || request_line =~ /favicon/
+#  puts request_line
+#
+#  client.puts "HTTP/1.1 200 OK"
+#  client.puts "Content-Type: text/plain\r\n\r\n"
+#
+#  http_method, path, params = parse_request(request_line)
+#  rolls = params["rolls"].to_i
+#  sides = params["sides"].to_i
+#
+#  rolls.times do
+#    roll = rand(sides)+1
+#    client.puts roll.to_s
+#  end
+#
+#  client.close
+#end
 
-  request_line = client.gets
+
+# Lecture Video: Sending a Complete Response
+
+# - previous code isnt returning valid response to browser
+# - were simply sending a text and not a proper HTTP response
+#   a valid response would include: status, headers (if any), body
+#   (bulk of data being sent back)
+# SOME NEGATIVES: we have HTML code and actual code altogether = MESSY
+#                 Sinatra will clarify this and help extract out the data.
+
+server = TCPServer.new("localhost", 3003)         # used a port # we're not using
+loop do
+  client = server.accept # waits until a request is made to server -> accepts it -> opens connection to client
+
+  request_line = client.gets # using gets to get first line of whatever the request is
   next if !request_line || request_line =~ /favicon/
   puts request_line
-
-  client.puts "HTTP/1.1 200 OK"
-  client.puts "Content-Type: text/plain\r\n\r\n"
-
+  next unless request_line
   http_method, path, params = parse_request(request_line)
+
+  client.puts "HTTP/1.1 200 OK"           # video added this line
+  client.puts "Content-Type: text/html"   # video modified this from plain to html
+  client.puts                             # adds empty line
+  client.puts "<html>"                    # print out html tag
+  client.puts "<body>"                    # print out body tag
+  client.puts "<pre>"                     # pre tag: preserve white space+print out as its seen
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts "</pre>"
+
+  client.puts "<h1>Rolls!</h1>"
   rolls = params["rolls"].to_i
   sides = params["sides"].to_i
 
   rolls.times do
     roll = rand(sides)+1
-    client.puts roll.to_s
+    client.puts "<p>", roll,"</p>"
   end
-
+  client.puts "</body>"                         # close body tag
+  client.puts "</html>"                         # close html tag
   client.close
 end
