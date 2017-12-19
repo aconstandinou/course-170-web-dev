a9a0010bbadea49093488af82033e08d5e0ecbf9
antoniozeus <constandinou.antonio@gmail.com>
2017-12-18 20:59:05 -0600

    LS Course 170

---
+++ working_with_sinatra/code_challenge_user_interests/users_interests.rb
@@ -0,0 +1,30 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+require "yaml"
+
+before do
+  @users_content = YAML.load_file("users.yaml")
+end
+
+helpers do
+  def count_interests
+    total_interests = 0
+    @users_content.each do |user, data|
+      total_interests += data[:interests].size
+    end
+    total_interests
+  end
+end
+
+get "/" do
+  @title = "List of Users"
+  erb :index
+end
+
+# adding a new route for user name
+get "/:name" do
+  @name = params[:name]
+
+  erb :user
+end
