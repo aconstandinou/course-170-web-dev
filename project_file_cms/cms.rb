fa8a995d40c413e13760a1d8ef12e99623e7fc2c
antoniozeus <constandinou.antonio@gmail.com>
2018-03-19 20:32:06 -0500

    new cms project code

---
+++ project_file_cms/cms.rb
@@ -0,0 +1,25 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+
+# Not found redirect
+not_found do
+  redirect "/"
+end
+
+root = File.expand_path("..", __FILE__)
+
+get "/" do
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
+  erb :index
+end
+
+get "/:filename" do
+  file_path = root + "/data/" + params[:filename]
+
+  headers["Content-Type"] = "text/plain"
+  File.read(file_path)
+end
